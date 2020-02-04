74f377c5d7e24d5b7e0c5681242ad358c9dcc91d
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-02-04 16:08:17 -0500

    Made Variables for Min and Max and did appropriate adjustments in sentence structure for those variables.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to number wizard.");
         Debug.Log("Pick a number bro.");
-        Debug.Log("The highest number you can pick is 1,000");
-        Debug.Log("The lowest number you can pick is only 1");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is only: " + min);
     }
 
     // Update is called once per frame
