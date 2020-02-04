fbb72f75e6d78b263f418a627e9ea59749f621dd
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-02-04 16:55:45 -0500

    Added Checks for when the Up, Down, and Enter keys are pressed.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -14,11 +14,24 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Pick a number bro.");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is only: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500.");
+        Debug.Log("Up Arrow = Up, Down Arrow = Down, Enter Key = Confirmation.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            Debug.Log("Down Arrow key was pressed."); 
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter Key was pressed.");
+        }
     }
 }
