25caa0ef0fcca75ae4942b7ff76d22e3d9477b6d
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-02-04 17:54:14 -0500

    Cleaned Up the Text, moved around the orders so that variables can be run properly without issue. Fixed bug with Max never reaching 1000.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -11,14 +11,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-
         Debug.Log("Welcome to number wizard.");
         Debug.Log("Pick a number bro.");
         Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is only: " + min);
-        Debug.Log("Tell me if your number is higher or lower than 500.");
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than..: " + guess);
         Debug.Log("Up Arrow = Up, Down Arrow = Down, Enter Key = Confirmation.");
+        max = max + 1;
     }
 
     // Update is called once per frame
@@ -26,19 +25,19 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Up Arrow key was pressed.");
             min = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than.." + guess);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) 
         {
-            Debug.Log("Down Arrow key was pressed.");
             max = guess;
-            Debug.Log(guess);
+            guess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than.." + guess);
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter Key was pressed.");
+            Debug.Log("Brilliant!");
         }
     }
 }
