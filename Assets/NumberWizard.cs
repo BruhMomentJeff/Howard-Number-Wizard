cef67308871a81b0284a6cc29d910ebd06339c1a
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-02-18 14:04:47 -0500

    Renamed Guess Variable to intGuess

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -6,7 +6,7 @@ public class NumberWizard : MonoBehaviour
 {
     int max;
     int min;
-    int guess;
+    int intGuess;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,13 @@ public class NumberWizard : MonoBehaviour
     {
         max = 1000;
         min = 1;
-        guess = 500;
+        intGuess = 500;
         
         Debug.Log("Welcome to number wizard.");
         Debug.Log("Pick a number bro.");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than..: " + guess);
+        Debug.Log("Tell me if your number is higher or lower than..: " + intGuess);
         Debug.Log("Up Arrow = Up, Down Arrow = Down, Enter Key = Confirmation.");
         max = max + 1;
     }
@@ -35,12 +35,12 @@ public class NumberWizard : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            min = guess;
+            min = intGuess;
             NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) 
         {
-            max = guess;
+            max = intGuess;
             NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
@@ -51,8 +51,8 @@ public class NumberWizard : MonoBehaviour
     }
     void NextGuess()
     {
-            guess = (max + min) / 2;
-            Debug.Log("Is it higher or lower than.." + guess);
+            intGuess = (max + min) / 2;
+            Debug.Log("Is it higher or lower than.." + intGuess);
         
     }
 }
